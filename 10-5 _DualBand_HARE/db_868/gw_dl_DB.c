6dc45681c5997b1a6c84818d335983c53a1aacb7
PradeepKumar <fernandomaurarivero@gmail.com>
2022-06-20 11:52:17 +0200

    make gw_dl_DB! Serious changes to polling: Poll child nodes by means of  nodeidDB, print JSON whether we receive or not

--- 10-5 _DualBand_HARE/db_868/gw_dl_DB.c
+++ 10-5 _DualBand_HARE/db_868/gw_dl_DB.c
@@ -139,6 +139,33 @@ childs_polled get_childs_ID(uint8_t nodeid_t, childs_polled childs)
     }
     return childs;
 }
+childs_polled get_childs_ID_m(uint8_t nodeid_t, childs_polled childs)
+{
+    
+    //printf("nodeid_t: %d\n", nodeid_t);
+    switch (nodeid_t)
+    {
+        case 1:
+            childs.nodeid1 = 1;
+            childs.nodeid2 = 2;
+            break;
+        case 2:
+            childs.nodeid1 = 3;
+            childs.nodeid2 = 4;
+            break;
+        case 3:
+            childs.nodeid1 = 5;
+            childs.nodeid2 = 6;
+            break;
+        case 4:
+            childs.nodeid1 = 7;
+            childs.nodeid2 = 8;
+            break;
+        default:
+            break;  
+    }
+    return childs;
+}
 
 static char *rxdata;
 static uint8_t bitmask;
@@ -204,6 +231,15 @@ PROCESS_THREAD(coordinator_process, ev,data)
     static clock_time_t t;
     static clock_time_t dt;
     static uint8_t pollbuf[2];
+
+    static uint8_t current_pollDB = 0; 
+
+    static struct flags_t {
+        uint8_t nodeDB1;
+        uint8_t nodeDB2;
+        uint8_t nodeDB3;
+        uint8_t nodeDB4;
+        } flags;
     
     PROCESS_BEGIN();
 
@@ -257,43 +293,108 @@ PROCESS_THREAD(coordinator_process, ev,data)
         PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&mm_timer));
 
         t=clock_time();
-        printf("seconds since boot: %lu\n", t/CLOCK_SECOND);
+        LOG_DBG("seconds since boot: %lu\n", t/CLOCK_SECOND);
         
         for(i = 1; i<9; i++)
         {
-           if(bitmask & 0x01){
-               bitmask = bitmask >> 1;
-                pollbuf[0] = 0b01100000; //poll is 3
-                pollbuf[1] = i;
-
-                nullnet_buf = (uint8_t*)&pollbuf;
-                nullnet_len = sizeof(pollbuf);
-
-                dt = clock_time() - t;
-                LOG_INFO("polling node %d, dt: %lu\n", i, dt/CLOCK_SECOND);
-               
-               NETSTACK_NETWORK.output(NULL); //POLLING IS BROADCAST, SHOULD BE UNICAST??
-
+            if(bitmask & 0x01){
+            bitmask = bitmask >> 1;
                 
+            switch(i){
+
+                case 1: 
+                case 2: 
+                    if(flags.nodeDB1 == 0){
+
+                        current_pollDB = 1;
+                        flags.nodeDB1 = 1;
+                        LOG_INFO("polling node DB1\n");
+                        pollbuf[0] = 0b01100000; //poll is 3
+                        pollbuf[1] = 1;
+                        nullnet_buf = (uint8_t*)&pollbuf;
+                        nullnet_len = sizeof(pollbuf);
+                        NETSTACK_NETWORK.output(NULL); 
+                    
+                    }
+                    else{
+                        LOG_DBG("node DB1 already polled\n");
+                    }
+                    break;
+
+                case 3: 
+                case 4:
+                    current_pollDB = 2;
+                    if(flags.nodeDB2 == 0){
+                        flags.nodeDB2 = 1;
+                        LOG_INFO("polling node DB2\n");
+
+                        pollbuf[0] = 0b01100000; //poll is 3
+                        pollbuf[1] = 2;
+                        nullnet_buf = (uint8_t*)&pollbuf;
+                        nullnet_len = sizeof(pollbuf);
+                        NETSTACK_NETWORK.output(NULL); 
+                    }
+                    else{
+                        LOG_DBG("node DB2 already polled\n");
+                    }
+                    break; 
+                case 5:
+                case 6:
+                    current_pollDB = 3;
+                    if(flags.nodeDB3 == 0){
+                        flags.nodeDB3 = 1;
+                        LOG_INFO("polling node DB3\n");
+                        pollbuf[0] = 0b01100000; //poll is 3
+                        pollbuf[1] = 3;
+                        nullnet_buf = (uint8_t*)&pollbuf;
+                        nullnet_len = sizeof(pollbuf);
+                        NETSTACK_NETWORK.output(NULL); 
+                    }   
+                    else{
+                        LOG_DBG("node DB3 already polled\n");
+                    }
+                    break;
+
+                case 7:
+                case 8:
+                    current_pollDB = 4;
+                    if(flags.nodeDB4 == 0){
+                        flags.nodeDB4 = 1;
+                        printf("polling node DB4\n");
+                        
+                        pollbuf[0] = 0b01100000; //poll is 3
+                        pollbuf[1] = 4;
+                        nullnet_buf = (uint8_t*)&pollbuf;
+                        nullnet_len = sizeof(pollbuf);
+                        NETSTACK_NETWORK.output(NULL); 
+                    }
+                    else{
+                        printf("node DB4 already polled\n");
+                    }
+                    break;
+            }                                
                 
                 etimer_set(&periodic_timer, T_SLOT); //set the timer for the next interval
                 PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
                 
                 if(!poll_response_received){
                     
-                    LOG_INFO("polling node %d, no response!! TRYING AGAIN \n", i);
-
-                    // HERE, TRY AGAIN? 
+                    LOG_DBG("polling node %d, no response!! TRYING AGAIN \n", current_pollDB);
+                    
                     NETSTACK_NETWORK.output(NULL);; 
                 }
 
                 etimer_set(&periodic_timer, T_GUARD);
                 PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
+
                 if(!poll_response_received){
 
-                    if(i<3){ //if we are in the first 3 nodes, account for error ( ONLY FOR TEST WITH 3 NODES)
+                    if(i<5){ //if we are in the first 3 nodes, account for error ( ONLY FOR TEST WITH 3 NODES)
 
-                        LOG_INFO("ERROR: POLL no RESPONSE!!\n");
+
+                        childs_polled nins_polled = get_childs_ID_m(current_pollDB, nins_polled);
+                        LOG_INFO("no response\n");
+                        printf(" { \"Nodeid_DB\": %d, \"nodeid_ch1\": %d, \"nodeid2_ch2\": %d, \"T1\": 0, \"H1\": 0, \"Pw_tx1\": 0,\"n_beacons1\": 0, \"n_transmissions1\": 0, \"permil_radio_on1\": 0,\"permil_tx1\": 0, \"permil_rx1\": 0, \"T2\": 0, \"H2\": 0, \"Pw_tx2\": 0, \"n_beacons2\": 0,  \"n_transmissions2\": 0, \"permil_radio_on2\": 0, \"permil_tx2\": 0, \"permil_rx2\": 0}\n" ,current_pollDB, nins_polled.nodeid1, nins_polled.nodeid2); 
                         lost_message_counter ++;
                     }
                     else{
@@ -308,7 +409,7 @@ PROCESS_THREAD(coordinator_process, ev,data)
               else{
                 bitmask = bitmask >> 1;
                 dt = clock_time() - t;
-                LOG_INFO("NOT polling node %d, dt: %lu\n", i, dt/CLOCK_SECOND);
+                LOG_INFO("NOT polling node %d, dt: %lu\n", current_pollDB, dt/CLOCK_SECOND);
                 etimer_set(&periodic_timer, T_SLOT); //set the timer for the next interval
                 PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
                 
@@ -317,6 +418,8 @@ PROCESS_THREAD(coordinator_process, ev,data)
                 
               }
         }
+        memset(&flags, 0, sizeof(flags));
+
         LOG_INFO("Polling finished\n");
       
          //if we need to change bitmask for next loop, do it here      
@@ -404,8 +507,7 @@ PROCESS_THREAD(callback_process,ev,data){
         uint32_t u32_var;
         uint8_t temp_array[4];
         } ua;
-    */
-    
+    */    
 
     union {
         int16_t u16_var;
