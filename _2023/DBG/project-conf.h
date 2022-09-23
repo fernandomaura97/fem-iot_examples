48ec390805864cee55f959cbb5e3e8b8c4c8f4cc
PradeepKumar <fernandomaurarivero@gmail.com>
2022-09-23 14:28:46 +0200

    [DBG]: Watchdog and NESTACK_RADIO.on

---
+++ _2023/DBG/project-conf.h
@@ -0,0 +1,12 @@
+
+#ifndef PROJECT_CONF_H_
+#define PROJECT_CONF_H_
+/*---------------------------*/
+//#define IEEE802154_CONF_DEFAULT_CHANNEL 17
+
+#define IEEE802154_CONF_DEFAULT_CHANNEL 15
+
+
+#define ENERGEST_CONF_ON 1
+
+#endif /* PROJECT_CONF_H_ */
